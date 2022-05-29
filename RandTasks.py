a0ddfbf430527bd12c181f7b1ddb672338cd8e77
Cam <64113264+RookWorks@users.noreply.github.com>
2022-05-29 11:23:16 -0400

    everything else works, I just need the user name to work

--- RandTasks.py
+++ RandTasks.py
@@ -7,19 +7,34 @@ def printpause(msg_to_print):
 
 def tasks (name, care, chore):
     while True:
-        user_input = input("What is your name?")
-        name.append(user_input)
-        printpause ("Hello " +name+ " would you like help deciding what to do today?")
+        user_input = input("What is your name?\n")
+        if user_input == "":
+            name.extend(user_input)
+            break        
+        user_input = input("Hello would you like help deciding" 
+                        " what to do today?\n")
         if user_input == "care":
-            printpause("" +care)
-        if user_input == "chore":
-            printpause("" +chore)
+            printpause("Maybe you should " +care+ "?\n")
+        elif user_input == "chore":
+            printpause("What if you " +chore+ "?\n")
+        playagain()
 
+def playagain():
+    again = input("Is that something you'd like to do? (y/n)\n")
+    if again == "y":
+        printpause("Happy to help! I'll be here if you need me.\n")
+        quit()
+    elif again == "n":
+        printpause == ("Hmm, maybe I can think of something else?\n")
+        run()
 
 def run():
     name = []
-    care = ([])
-    chore = ([])
+    care = random.choice(["try to meditate for 30 minutes", "take a 30 minute cat nap", 
+            "read or listen to an audio book", "take a short stroll", 
+            "drink some water", "eat a snack", "message a friend"])
+    chore = random.choice(["vaccum one room", "make your bed", "sweep the kitchen",
+            "water your garden", "do your laundry", "clean your desk"])
     tasks (name, care, chore)
 
 run()
