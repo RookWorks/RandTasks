fc9a631c3fd1c8c29851891f0817f81cc5b24f9c
Cam <64113264+RookWorks@users.noreply.github.com>
2022-05-29 10:37:29 -0400

    just running the logic now

--- RandTasks.py
+++ RandTasks.py
@@ -5,6 +5,17 @@ def printpause(msg_to_print):
     print(msg_to_print)
     time.sleep(2)
 
+def tasks (name, care, chore):
+    while True:
+        user_input = input("What is your name?")
+        name.append(user_input)
+        printpause ("Hello " +name+ " would you like help deciding what to do today?")
+        if user_input == "care":
+            printpause("" +care)
+        if user_input == "chore":
+            printpause("" +chore)
+
+
 def run():
     name = []
     care = ([])
